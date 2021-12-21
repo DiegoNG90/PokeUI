dbb50b7c0ccdb0328314100fceaf61bfbfbc990d
Diego <diego.nicolas.gandara@gmail.com>
2021-12-20 23:27:38 -0300

    The Home page will manage the state of a initial request of 151 pokemons (which will be displayed by child component PokemonContainer) and also the targetSerachedPokemon, which will be determined by the SearchBar child component through the findPokemon function. Also: if useFetch loads or error, it will through a basic title warning.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,29 @@
+import { useState, useEffect } from 'react';
+import { useFetch } from '../hooks/useFetch';
 import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
+import Head from 'next/head';
+import SearchBar from '../components/SearchBar';
+import PokemonsContainer from '../components/PokemonsContainer';
+
 const Home: NextPage = () => {
+  // const [endpoint, setEndpoint] = useState<string>('pokemon');
+  const [targetSearchedPokemon, setTargetSearchedPokemon] = useState('');
+  const { data, loading, error } = useFetch(`pokemon?limit=151` /*endpoint*/);
+  const { results } = data || [];
+
+  console.log('Results? :>', results);
+
+  const findPokemon = (pokemon: string): void => {
+    const [targetPokemon] = results.filter((p) => p.name === pokemon);
+    const { url } = targetPokemon || {};
+    setTargetSearchedPokemon(url ? url : '');
+  };
+
+  if (loading) return <h1>Loading</h1>;
+  if (error) return <h2>Error</h2>;
+
   return (
     <div className={styles.container}>
       <Head>
@@ -18,49 +38,13 @@ const Home: NextPage = () => {
           El que quiere Pokemons, que los busque!
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        <SearchBar findPokemon={findPokemon} />
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <PokemonsContainer
+          targetSearchedPokemon={targetSearchedPokemon}
+          results={results}
+        />
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   );
 };
