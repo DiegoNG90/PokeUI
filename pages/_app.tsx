15ef198afb91915afba8820c9608ea51fc04dc2d
Diego <diego.nicolas.gandara@gmail.com>
2021-12-27 22:50:28 -0300

    Import and wrap all the MyApp component with the <DataProvider>, meaning that inside any component of our app, we can get/access any of those 151 pokemons data.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import type { AppProps } from 'next/app';
 
 import Layout from '../components/Layout/Layout';
+import { DataProvider } from '../context/dataContext';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <DataProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </DataProvider>
   );
 }
 
